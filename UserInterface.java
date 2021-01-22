89339c5c38b75e650e486ec3988d46555c376847
Stefan Shivarov <71455791+StefanShivarov@users.noreply.github.com>
2021-01-22 21:04:37 +0200

    Commit 5

--- UserInterface.java
+++ UserInterface.java
@@ -1,18 +1,24 @@
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.List;
 import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import javax.swing.*;
 
 public class UserInterface {
 
 	private JFrame frame;
-	private JTextField bookField;
 	private JTextField discountCodeField;
-	private JTextField priceField;
+	double sum = 0;
+	ArrayList<Book> boughtBooks = new ArrayList<>();
 
 	/**
 	 * Launch the application.
@@ -42,7 +48,7 @@ public class UserInterface {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.getContentPane().setBackground(new Color(255, 239, 213));
+		frame.getContentPane().setBackground(new Color(216, 191, 216));
 		frame.setBounds(100, 100, 700, 550);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Shopping Cart Menu");
@@ -51,33 +57,63 @@ public class UserInterface {
 		
 		JLabel bookNameLabel = new JLabel("Book name:");
 		bookNameLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		bookNameLabel.setBounds(27,64,100,15);
+		bookNameLabel.setBounds(22,65,100,15);
 		frame.getContentPane().add(bookNameLabel);
 		
-		JLabel priceLabel = new JLabel("Price:");
+		JLabel prLbl = new JLabel("Book price:");
+		prLbl.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		prLbl.setBounds(27, 132, 80, 15);
+		frame.getContentPane().add(prLbl);
+
+		JLabel priceLabel = new JLabel("");
 		priceLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		priceLabel.setBounds(27, 132, 50, 15);
+		priceLabel.setBounds(113, 132, 100, 16);
 		frame.getContentPane().add(priceLabel);
+
+		BookStore bookStore = new BookStore();
+		bookStore.fillBooksInfo(new File("C:\\Users\\Stefan Jr\\Desktop\\books.txt")
+				, new File("C:\\Users\\Stefan Jr\\Desktop\\prices.txt"));
+		ArrayList<String> booksList = bookStore.getBookTitles();
 		
-		bookField = new JTextField();
-		bookField.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		bookField.setBounds(121, 61, 527, 22);
-		frame.getContentPane().add(bookField);
+		
+		JComboBox comboBox = new JComboBox(booksList.toArray());
+		comboBox.setSelectedIndex(-1);
+		comboBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if(comboBox.getSelectedIndex()!=-1) {
+					String[]bookInfoArr = String.valueOf(comboBox.getSelectedItem()).split(" - ");
+					String title = bookInfoArr[0];
+					Book currentBook = bookStore.getBookByTitle(title);
+					double price = currentBook.getPrice();
+					priceLabel.setText(String.format("%.2f lv.", price));
+				}
+			}
+		});
+		comboBox.setBounds(132, 63, 516, 22);
+		frame.getContentPane().add(comboBox);
 		
 		discountCodeField = new JTextField();
-		discountCodeField.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		discountCodeField.setBounds(485, 129, 163, 22);
+		discountCodeField.setBounds(432, 131, 216, 22);
+		discountCodeField.setColumns(10);
 		frame.getContentPane().add(discountCodeField);
+		discountCodeField.setVisible(false);
 		
-		JLabel discountCodeLabel = new JLabel("Discount code (if you have one): ");
-		discountCodeLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		discountCodeLabel.setBounds(247, 129, 241, 20);
-		frame.getContentPane().add(discountCodeLabel);
 		
-		priceField = new JTextField();
-		priceField.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		priceField.setBounds(82, 129, 150, 22);
-		frame.getContentPane().add(priceField);
+		JCheckBox discountCheck = new JCheckBox("Use dicount code");
+		discountCheck.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				discountCodeField.setVisible(true);
+				if(!discountCheck.isSelected()) {
+					discountCodeField.setVisible(false);
+				}
+			}
+		});
+		discountCheck.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		discountCheck.setBackground(new Color(216, 191, 216));
+		discountCheck.setBounds(270, 130, 156, 23);
+		frame.getContentPane().add(discountCheck);
 		
 		TextArea textArea = new TextArea();
 		textArea.setEnabled(true);
@@ -87,18 +123,19 @@ public class UserInterface {
 		textArea.setBounds(205, 202, 443, 285);
 		frame.getContentPane().add(textArea);
 		
+		
 		JButton addToCartButton = new JButton("Add to Cart");
 		addToCartButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		addToCartButton.setBounds(21, 202, 164, 57);
 		addToCartButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				double price = Double.parseDouble(priceField.getText());
-				String[] bookArr = bookField.getText().split(" - ");
-				String bookName = bookArr[0];
-				String bookAuthor = bookArr[1];
-				textArea.append(String.format("Added %s by %s to the cart -----> %.2f lv.%n", bookName, bookAuthor, price));
-				
+				String[]bookInfoArr = String.valueOf(comboBox.getSelectedItem()).split(" - ");
+				String title = bookInfoArr[0];
+				Book addedBook = bookStore.getBookByTitle(title);
+				sum+=addedBook.getPrice();
+				textArea.append(String.format("Added %s to the cart.%n",addedBook.toString()));
+				boughtBooks.add(addedBook);
 			}
 		});
 		frame.getContentPane().add(addToCartButton);
@@ -109,10 +146,12 @@ public class UserInterface {
 		emptyCartButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				bookField.setText("");
+				comboBox.setSelectedIndex(-1);
 				discountCodeField.setText("");
-				priceField.setText("");
+				priceLabel.setText("");
 				textArea.setText("");
+				discountCheck.setSelected(false);
+				discountCodeField.setVisible(false);
 				
 			}
 		});
@@ -125,20 +164,27 @@ public class UserInterface {
 		checkoutButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				if(!discountCodeField.getText().isBlank()) {
-					int discountPercentage = Integer.parseInt(discountCodeField.getText()
-						.substring(discountCodeField.getText().length()-2,
-								discountCodeField.getText().length()));
-				
-					textArea.append(String.format("Discount code \"%s\" successfully added! -> %d%c discount!",
-							discountCodeField.getText(), discountPercentage, '%'));
+				textArea.append("----------------------CHECKOUT----------------------\n");
+				int pos = 1;
+				for(Book b : boughtBooks) {
+					textArea.append(String.format("%d) \"%s\" - %s -----> %.2f lv.\n",pos, b.getTitle(), b.getAuthor(), b.getPrice()));
+					pos++;
+				}
+				if(discountCheck.isSelected()) {
+					
+					double discountPercentage = Double.parseDouble(discountCodeField.getText()
+							.substring(discountCodeField.getText().length()-2, discountCodeField.getText().length()));
+					
+					sum*=(1-discountPercentage/100);
+					textArea.append(String.format("Discount code \"%s\" used -----> - %.0f%c\n"
+							,discountCodeField.getText(), discountPercentage, '%'));
 				}
 				
+				textArea.append("----------------------------------------------------\n");
+				textArea.append(String.format("TOTAL : %.2f lv.", sum));
 				
 			}
 		});
 		frame.getContentPane().add(checkoutButton);
-		
 	}
-
 }
